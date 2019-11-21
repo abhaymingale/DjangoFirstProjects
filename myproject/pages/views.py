f4ec0548b6e2595e9b98f91ea6d272d8da801b7a
Abhay Ingale <abhaymingale@gmail.com>
2019-11-21 23:34:33 +0530

    New Search Home page and about us

--- myproject/pages/views.py
+++ myproject/pages/views.py
@@ -2,12 +2,28 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from listings.models import Listing
+
+from realtors.models import Realtors
+
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'pages/index.html')
+    listing = Listing.objects.order_by('-List_date').filter()[:3]
+    context = {
+        'listing': listing
+    }
+    print(context)
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    single_realtor = Realtors.objects.filter(is_mvp=True)
+    all_realtor = Realtors.objects.all()
+    context = {
+        'singlerealtor': single_realtor[0],
+        'all_realtor': all_realtor
+    }
+    print(context)
+    return render(request, 'pages/about.html', context)
